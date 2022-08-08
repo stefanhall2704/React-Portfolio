4210bcb1eb1585e94cf697995767e2fc647d609f
Stefan Hall <stefan.hall@cfainstitute.org>
2022-08-07 21:09:53 -0400

    api's working but not on main server, cannot trigger api's from UI

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,20 @@ import Axios from 'axios'
 
 
 function Application() {
-  const [project_title, set_project_title] = useState('');
-  const [project_link, set_project_link] = useState('');
-  const [user_id, set_user_id] = useState('');
-  const submitReview = ()=> {
-    Axios.post('http://localhost:3001/api/project/create', {
-      project_title: project_title, 
-      project_link: project_link, 
-      user_id: user_id
+  const [first_name, set_first_name] = useState('');
+  const [last_name, set_last_name] = useState('');
+  const [email, set_email] = useState('');
+  const [password, set_password] = useState('');
+  const [phone_number, set_phone_number] = useState('');
+;  const submitReview = ()=> {
+    Axios.post('http://localhost:3001/api/user/create', {
+      first_name: first_name, 
+      last_name: last_name, 
+      email: email,
+      password: password,
+      phone_number: phone_number
     }).then(()=>{
-      alert('successful insert');
+      alert(`User ${first_name} ${last_name} added.`);
     }).catch((err)=> {
       console.log(err);
     })
@@ -28,25 +32,37 @@ function Application() {
               <div>
                   <div className="card-body">
                       <div className="form-group">
-                          <label>Project Title</label>
+                          <label>First Name</label>
                           <input 
                           type="text" 
-                          id="projectTitle" 
+                          id="firstName" 
                           onChange={(e)=> {
-                            set_project_title(e.target.value);
+                            set_first_name(e.target.value);
                           }}
                           />
                       </div>
                       <div className="form-group">
-                          <label>Project URL</label>
-                          <input type="text" id="projectLink" onChange={(e)=> {
-                            set_project_link(e.target.value);
+                          <label>Last Name</label>
+                          <input type="text" id="lastName" onChange={(e)=> {
+                            set_last_name(e.target.value);
                           }}/>
                       </div>
                       <div className="form-group">
-                        <label>User</label><br />
-                          <input type="number" onChange={(e)=> {
-                            set_user_id(e.target.value);
+                          <label>Email</label>
+                          <input type="email" id="email" onChange={(e)=> {
+                            set_email(e.target.value);
+                          }}/>
+                      </div>
+                      <div className="form-group">
+                        <label>Password</label><br />
+                          <input type="password" id="password" onChange={(e)=> {
+                            set_password(e.target.value);
+                          }}/>
+                      </div>
+                      <div className="form-group">
+                          <label>Phone Number</label>
+                          <input type="tel" id="phoneNumber" onChange={(e)=> {
+                            set_phone_number(e.target.value);
                           }}/>
                       </div>
                     <button onClick={submitReview}>Submit</button>
